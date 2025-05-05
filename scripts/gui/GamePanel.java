390a87c978300543386d1c792f7bf69977f4cdf8
Mohamed Morsy <2014905@apps.nsd.org>
2025-05-05 14:19:47 -0700

    Co-authored-by: GauthamM08 <GauthamM08@users.noreply.github.com>

--- scripts/gui/GamePanel.java
+++ scripts/gui/GamePanel.java
@@ -2,13 +2,34 @@ package scripts.gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import javax.swing.Timer;
+
+import javax.imageio.ImageIO;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class GamePanel extends JPanel implements KeyListener {
+    private BufferedImage bg;
     private final ArrayList<Ball> gameBalls = new ArrayList<>();
+    protected static JSlider shotStrengthSlider;
+    private static final int STRENGTH_MIN = 0;
+    private static final int STRENGTH_MAX = 1000;
+    private static final int STRENGTH_INIT = 10;
+    private boolean isHoldingDown = false;
+    private boolean lockSlider = false;
 
     public GamePanel() {
         System.out.println("Game Panel initialized.");
@@ -54,17 +75,79 @@ public class GamePanel extends JPanel implements KeyListener {
    }
 
     private void initProperties() {
-        this.setBackground(Color.black);
+        this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.addKeyListener(this);
+        shotStrengthSlider = new JSlider(JSlider.VERTICAL, STRENGTH_MIN, STRENGTH_MAX, STRENGTH_INIT);
+        shotStrengthSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                System.out.println("Strength updated.");
+            }
+        });
+
+        Hashtable strengthLabelTable = new Hashtable();
+        strengthLabelTable.put(0, new JLabel("a"));
+        strengthLabelTable.put(STRENGTH_INIT, new JLabel("b"));
+        strengthLabelTable.put(STRENGTH_MAX, new JLabel("c"));
+        shotStrengthSlider.setLabelTable(strengthLabelTable);
+        shotStrengthSlider.setPaintLabels(true);
+        shotStrengthSlider.setPaintTicks(false);
+        shotStrengthSlider.setEnabled(false);
+        shotStrengthSlider.setOpaque(true);
+
+        this.add(shotStrengthSlider);
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == 1)
+                    isHoldingDown = true;
+                
+                if (e.getButton() == 3) {
+                    lockSlider = true;
+                    isHoldingDown = false;
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                isHoldingDown = false;
+            }
+        });
+
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        try {
+            bg = ImageIO.read(new File("assets/game_assets/Background Board.png"));
+        } catch (Exception e) {
+        }
+        g.drawImage(bg, 0, 0, this);
+        
 
-        for (Ball b : gameBalls) {
-            if (b.isActive()) b.drawSprite(g);
+        if (isHoldingDown) {
+            shotStrengthSlider.setValue(shotStrengthSlider.getValue() + 5);
+        }
+        else if (lockSlider && !isHoldingDown) {
+            Timer t = new Timer(1000, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    shotStrengthSlider.setValue(STRENGTH_MIN);
+                    lockSlider = false;        
+                }
+            });
+
+            t.setRepeats(false);
+            t.start();
+        }
+        else {
+            shotStrengthSlider.setValue(shotStrengthSlider.getValue() - 10);
+            for (Ball b : gameBalls) {
+                if (b.isActive()) b.drawSprite(g);
+            }
         }
     }
 }
